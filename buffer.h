c6fb37843634427e94324055a3a6b76b22519ece
Nicholas Carroll <nicholascarroll@tutanota.com>
2026-04-10 18:53:12 +1000

    Extracted word wrap functions from buffer.c to wrap.c   - wrap.h transitive include in buffer.h should go in the consumers instead

--- buffer.h
+++ buffer.h
@@ -1,6 +1,7 @@
 #ifndef EMIL_BUFFER_H
 #define EMIL_BUFFER_H
 #include "emil.h"
+#include "wrap.h"
 void insertRow(struct buffer *bufr, int at, char *s, size_t len);
 void freeRow(erow *row);
 void delRow(struct buffer *bufr, int at);
@@ -16,20 +17,6 @@ void nextBuffer(void);
 void previousBuffer(void);
 void killBuffer(void);
 void computeDisplayNames(void);
-void invalidateScreenCache(struct buffer *buf);
-void buildScreenCache(struct buffer *buf, int screencols);
-int getScreenLineForRow(struct buffer *buf, int row, int screencols);
-int calculateLineWidth(erow *row);
-int charsToDisplayColumn(erow *row, int char_pos);
-int countScreenLines(erow *row, int screencols);
-int wordWrapBreak(erow *row, int screencols, int line_start_col,
-		  int line_start_byte, int *break_col, int *break_byte);
-void cursorScreenLine(erow *row, int cursor_col, int screencols, int *out_line,
-		      int *out_col);
-int sublineBounds(erow *row, int screencols, int target_subline,
-		  int *start_byte, int *end_byte);
-int displayColumnToByteOffset(erow *row, int screencols, int target_subline,
-			      int target_col);
 void clampToBuffer(struct buffer *buf, int *px, int *py);
 void clampPositions(struct buffer *buf);
 
